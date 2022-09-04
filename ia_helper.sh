de61e4a7fb1067164f77cd0e487e1af799ac33f7
jacob-jarick <mem.namefix@gmail.com>
2022-09-05 02:00:22 +0800

    Update ia_helper.sh

--- ia_helper.sh
+++ ia_helper.sh
@@ -6,14 +6,16 @@ PREFIX=$3 # optional internet archive directory prefix
 
 # grep ARGUMENTS
 
-# -v        Exclude matches
-# '^\s*$'   Match any empty lines
+# -v          Exclude matches
+# -e '^\s*$'  Match any empty lines
+# -e '^#'     Match lines beginning with #
 
 # parallel ARGUMENTS
 
-# --line-buffer               show output while working
-# --trim lr                   remove leading and trailing whitespaces from entry
-# --halt now,fail=1           exit on any error
-# -k                          keep order
+# --jobs 2            run up to 2 downloads in parallel
+# --line-buffer       show output while working
+# --trim lr           remove leading and trailing whitespaces from entry
+# --halt now,fail=1   exit on any error
+# -k                  keep order
 
-grep -v -e '^\s*$' -e '^#' $FILE | parallel --line-buffer --trim lr --halt now,fail=1 -k ia download $ARCHIVE "$PREFIX{}"
+grep -v -e '^\s*$' -e '^#' $FILE | parallel --jobs 2 --line-buffer --trim lr --halt now,fail=1 -k ia download $ARCHIVE "$PREFIX{}"
