c626742572286a2c4e0adf0920a3a62e7093b875
jacob-jarick <mem.namefix@gmail.com>
2022-09-04 22:33:43 +0800

    Update ia_helper.sh

--- ia_helper.sh
+++ ia_helper.sh
@@ -3,4 +3,9 @@
 ARCHIVE=$1
 FILE=$2
 
-grep -v '^\s*$' $FILE | xargs -d'\n' -I{} ia download $ARCHIVE {}
+# old xargs implementation
+#grep -v '^\s*$' $FILE | xargs -d'\n' -I{} ia download $ARCHIVE {}
+
+# new parallel implementation
+grep -v -e '^\s*$' $FILE | parallel --halt now,fail=1 -k ia download $ARCHIVE {}
+
